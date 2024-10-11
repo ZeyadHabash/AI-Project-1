2ce211d2c6adbefeac23fa058f0d3e08724fe77b
Zeyad Habash <zeyad.habashy02@gmail.com>
2024-10-11 14:51:06 +0300

    Move nodesexpanded to WaterSortSearch

--- src/code/Driver.java
+++ src/code/Driver.java
@@ -6,11 +6,14 @@ package code;
 public class Driver {
 
     public static void main(String[] args) throws InterruptedException {
-        // System.out.println(WaterSortSearch.solve("5;4;b,y,r,b;b,y,r,r;y,r,b,y;e,e,e,e;e,e,e,e;", "BF", false));
-        System.out.println(WaterSortSearch.solve("5;4;b,y,r,b;b,y,r,r;y,r,b,y;e,e,e,e;e,e,e,e;", "BF", false));
-        System.out.println(WaterSortSearch.solve("5;4;b,y,r,b;b,y,r,r;y,r,b,y;e,e,e,e;e,e,e,e;", "DF", false));
-        System.out.println(WaterSortSearch.solve("5;4;b,y,r,b;b,y,r,r;y,r,b,y;e,e,e,e;e,e,e,e;", "UC", false));
-        System.out.println(WaterSortSearch.solve("5;4;b,y,r,b;b,y,r,r;y,r,b,y;e,e,e,e;e,e,e,e;", "AS2", false));
-
+        String initialState = "5;4;b,y,r,b;b,y,r,r;y,r,b,y;e,e,e,e;e,e,e,e;";
+        System.out.println(WaterSortSearch.solve(initialState, "BF", false));
+        System.out.println(WaterSortSearch.solve(initialState, "DF", false));
+        System.out.println(WaterSortSearch.solve(initialState, "UC", false));
+        System.out.println(WaterSortSearch.solve(initialState, "ID", false));
+        System.out.println(WaterSortSearch.solve(initialState, "GR1", false));
+        System.out.println(WaterSortSearch.solve(initialState, "GR2", false));
+        System.out.println(WaterSortSearch.solve(initialState, "AS1", false));
+        System.out.println(WaterSortSearch.solve(initialState, "AS2", false));
     }
 }
