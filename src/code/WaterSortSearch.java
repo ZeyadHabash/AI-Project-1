f007a800be582b576e36b654923756f9be3c7ffd
mazen <mazzen.m.s0liman@gmail.com>
2024-10-15 01:26:30 +0300

    Fixed a bug in Heuristic 2

--- src/code/WaterSortSearch.java
+++ src/code/WaterSortSearch.java
@@ -662,6 +662,7 @@ public class WaterSortSearch extends GenericSearch {
         for (int i = 0; i < state.arrayOfTubes.length; i++) {
             copiedArrayOfTubes[i] = state.arrayOfTubes[i].clone();
         }
+
         // create an array that contains the count of the number colored layers in every bottle
         int[] arrayOfNumberOfColoredLayers = new int[state.numOfBottles];
 
@@ -670,10 +671,10 @@ public class WaterSortSearch extends GenericSearch {
 
         // get the count of the number of colored layers in every bottle
         for (int i = 0; i < state.arrayOfTopPointers.length; i++) {
-            arrayOfNumberOfColoredLayers[i] = state.arrayOfTopPointers[i] + 1;
+            arrayOfNumberOfColoredLayers[i] = state.bottleCapacity - state.arrayOfTopPointers[i];
         }
 
-        // traverse over the array ot bottles
+        // traverse over the array of bottles
         for (int i = 0; i < copiedArrayOfTubes.length; i++) {
             // get the color label of the current bottle
             char label = arrayOfColoredLabels[i].first;
